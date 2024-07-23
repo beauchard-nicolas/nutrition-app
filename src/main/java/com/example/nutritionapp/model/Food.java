cfe16a1e25abafcccd78c7478ec3460dc8318338
Archok <beauchard.nicolas@gmail.com>
2024-07-23 23:52:49 +0200

    Ajout de la classe Food

--- src/main/java/com/example/nutritionapp/model/Food.java
+++ src/main/java/com/example/nutritionapp/model/Food.java
@@ -1,4 +1,30 @@
 package com.example.nutritionapp.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "foods")
 public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Double calories; // par 100g
+    private Double proteins; // en g par 100g
+    private Double carbs;    // en g par 100g
+    private Double fats;     // en g par 100g
+    private Double fiber;    // en g par 100g
+
+    @Enumerated(EnumType.STRING)
+    private FoodCategory category;
+
+    public enum FoodCategory {
+        FRUIT, VEGETABLE, GRAIN, PROTEIN, DAIRY, SNACK, BEVERAGE, OTHER
+    }
 }
