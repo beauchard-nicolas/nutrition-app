0d396730c6ab72d2cac93114bc26e3ab5bf8a11d
Archok <beauchard.nicolas@gmail.com>
2024-07-24 00:05:24 +0200

    feat(repository): Add JPA repositories for all entities
    - Create UserRepository
    - Create FoodRepository
    - Create MealRepository
    - Create ShoppingListRepository
    - Add basic finder methods for each repository

--- src/main/java/com/example/nutritionapp/repository/UserRepository.java
+++ src/main/java/com/example/nutritionapp/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.nutritionapp.repository;
 
-public interface UserRepository {
+import com.example.nutritionapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
